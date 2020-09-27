ad8db105f87aa686318a395f94312aae09cfa74c
Florian Thienel <florian@thienel.org>
2020-09-27 16:16:42 +0200

    remove dependency on implemented interface to be more ideomatic

--- core/clock/clock.go
+++ core/clock/clock.go
@@ -2,32 +2,30 @@ package clock
 
 import (
 	"time"
-
-	"github.com/ftl/hellocontest/core"
 )
 
 // New returns a new clock that runs on system time.
-func New() core.Clock {
-	return new(defaultClock)
+func New() *DefaultClock {
+	return new(DefaultClock)
 }
 
 // Static returns a new clock that always returns the given static value.
-func Static(time time.Time) core.Clock {
-	return &staticClock{
+func Static(time time.Time) *StaticClock {
+	return &StaticClock{
 		time: time,
 	}
 }
 
-type defaultClock struct{}
+type DefaultClock struct{}
 
-func (defaultClock) Now() time.Time {
+func (*DefaultClock) Now() time.Time {
 	return time.Now()
 }
 
-type staticClock struct {
+type StaticClock struct {
 	time time.Time
 }
 
-func (clock staticClock) Now() time.Time {
-	return clock.time
+func (c *StaticClock) Now() time.Time {
+	return c.time
 }

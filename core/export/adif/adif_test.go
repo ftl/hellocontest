dce0a5f634159eea8324f733b02c9e6120bcff77
Florian Thienel <florian@thienel.org>
2020-12-07 21:22:58 +0100

    add the frequency to the adif tests

--- core/export/adif/adif_test.go
+++ core/export/adif/adif_test.go
@@ -7,9 +7,10 @@ import (
 	"time"
 
 	"github.com/ftl/hamradio/callsign"
-	"github.com/ftl/hellocontest/core"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"github.com/ftl/hellocontest/core"
 )
 
 func TestData(t *testing.T) {
@@ -40,6 +41,23 @@ func TestRecord(t *testing.T) {
 		expected string
 	}{
 		{
+			desc: "80m RTTY with Frequency",
+			qso: core.QSO{
+				Callsign:     theirCall,
+				Time:         time.Date(2009, time.May, 30, 0, 2, 0, 0, time.UTC),
+				Frequency:    3550000,
+				Band:         core.Band80m,
+				Mode:         core.ModeRTTY,
+				MyReport:     core.RST("599"),
+				MyNumber:     core.QSONumber(1),
+				MyXchange:    "ABC",
+				TheirReport:  core.RST("589"),
+				TheirNumber:  core.QSONumber(4),
+				TheirXchange: "DEF",
+			},
+			expected: "<QSO_DATE:8>20090530<TIME_ON:4>0002<TIME_OFF:4>0002<CALL:4>S50A<FREQ:5>3.550<BAND:3>80m<MODE:4>RTTY<RST_SENT:3>599<RST_RCVD:3>589<COMMENT:15>001 ABC 004 DEF<EOR>\n",
+		},
+		{
 			desc: "40m CW",
 			qso: core.QSO{
 				Callsign:     theirCall,
@@ -53,7 +71,7 @@ func TestRecord(t *testing.T) {
 				TheirNumber:  core.QSONumber(4),
 				TheirXchange: "DEF",
 			},
-			expected: "<QSO_DATE:8>20090530<TIME_ON:4>0002<TIME_OFF:4>0002<CALL:4>S50A<BAND:3>40m<MODE:2>CW<RST_SENT:3>599<RST_RCVD:3>589<COMMENT:15>001 ABC 004 DEF<EOR>\n",
+			expected: "<QSO_DATE:8>20090530<TIME_ON:4>0002<TIME_OFF:4>0002<CALL:4>S50A<FREQ:5>7.000<BAND:3>40m<MODE:2>CW<RST_SENT:3>599<RST_RCVD:3>589<COMMENT:15>001 ABC 004 DEF<EOR>\n",
 		},
 		{
 			desc: "20m SSB",
@@ -69,7 +87,7 @@ func TestRecord(t *testing.T) {
 				TheirNumber:  core.QSONumber(4),
 				TheirXchange: "YYY",
 			},
-			expected: "<QSO_DATE:8>20090530<TIME_ON:4>0002<TIME_OFF:4>0002<CALL:4>S50A<BAND:3>20m<MODE:3>SSB<RST_SENT:2>59<RST_RCVD:2>58<COMMENT:15>001 XXX 004 YYY<EOR>\n",
+			expected: "<QSO_DATE:8>20090530<TIME_ON:4>0002<TIME_OFF:4>0002<CALL:4>S50A<FREQ:6>14.000<BAND:3>20m<MODE:3>SSB<RST_SENT:2>59<RST_RCVD:2>58<COMMENT:15>001 XXX 004 YYY<EOR>\n",
 		},
 	}
 	for _, tC := range testCases {
@@ -102,7 +120,7 @@ func TestExport(t *testing.T) {
 <adif_ver:5>3.0.9
 <programid:11>HelloContest
 <EOH>
-<QSO_DATE:8>20090530<TIME_ON:4>0002<TIME_OFF:4>0002<CALL:4>S50A<BAND:3>40m<MODE:2>CW<RST_SENT:3>599<RST_RCVD:3>589<COMMENT:15>001 ABC 004 DEF<EOR>
+<QSO_DATE:8>20090530<TIME_ON:4>0002<TIME_OFF:4>0002<CALL:4>S50A<FREQ:5>7.000<BAND:3>40m<MODE:2>CW<RST_SENT:3>599<RST_RCVD:3>589<COMMENT:15>001 ABC 004 DEF<EOR>
 `
 
 	Export(buffer, qso)

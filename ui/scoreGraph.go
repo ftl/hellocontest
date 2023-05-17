392dabd092ceccdea5428f465ab3f14f71de7f8e
Florian Thienel <ftl@thecodingflow.com>
2023-05-17 18:47:50 +0200

    extract method that draws the raw score graph for a set of data points

--- ui/scoreGraph.go
+++ ui/scoreGraph.go
@@ -50,112 +50,82 @@ func (g *scoreGraph) UpdateTimeFrame() {
 	g.timeFrameIndex = g.graph.Bindex(time.Now())
 }
 
+type graphLayout struct {
+	width         float64
+	height        float64
+	marginY       float64
+	zeroY         float64
+	maxHeight     float64
+	lowZoneHeight float64
+	binWidth      float64
+}
+
 func (g *scoreGraph) Draw(da *gtk.DrawingArea, cr *cairo.Context) {
 	cr.Save()
 	defer cr.Restore()
 
 	// preparations
 
-	// TODO extract graph to separate type and use parameters for width, height, marginY
-	width := float64(da.GetAllocatedWidth())
-	height := float64(da.GetAllocatedHeight())
-	marginY := 5.0
-
-	zeroY := height / 2
-	maxHeight := zeroY - marginY
-	lowZoneHeight := maxHeight / 2
-
 	valueCount := len(g.graph.DataPoints)
-	binWidth := width / float64(valueCount)
+	layout := g.calculateLayout(da, valueCount)
 
 	// the background
 	g.fillBackground(cr)
 
 	// the zone
 	cr.SetSourceRGBA(rateStyle.lowZoneColor.toRGBA(rateStyle.areaAlpha))
-	cr.MoveTo(0, zeroY-lowZoneHeight)
-	cr.LineTo(width, zeroY-lowZoneHeight)
-	cr.LineTo(width, zeroY+lowZoneHeight)
-	cr.LineTo(0, zeroY+lowZoneHeight)
+	cr.MoveTo(0, layout.zeroY-layout.lowZoneHeight)
+	cr.LineTo(layout.width, layout.zeroY-layout.lowZoneHeight)
+	cr.LineTo(layout.width, layout.zeroY+layout.lowZoneHeight)
+	cr.LineTo(0, layout.zeroY+layout.lowZoneHeight)
 	cr.ClosePath()
 	cr.Fill()
 
 	cr.SetSourceRGBA(rateStyle.lowZoneColor.toRGBA(rateStyle.borderAlpha))
-	cr.MoveTo(0, zeroY-lowZoneHeight)
-	cr.LineTo(width, zeroY-lowZoneHeight)
-	cr.LineTo(width, zeroY+lowZoneHeight)
-	cr.LineTo(0, zeroY+lowZoneHeight)
+	cr.MoveTo(0, layout.zeroY-layout.lowZoneHeight)
+	cr.LineTo(layout.width, layout.zeroY-layout.lowZoneHeight)
+	cr.LineTo(layout.width, layout.zeroY+layout.lowZoneHeight)
+	cr.LineTo(0, layout.zeroY+layout.lowZoneHeight)
 	cr.ClosePath()
 	cr.Stroke()
 
 	// the graph
-
-	// cr.SetSourceRGBA(0, 0, 1, 0.6) // TODO calculate the overall achievement and use the corresponding color
-	cr.SetSourceRGB(rateStyle.scoreGraphColor.toRGB()) // TODO calculate the overall achievement and use the corresponding color
-	cr.MoveTo(0, zeroY)
-
-	valueScaling := lowZoneHeight / g.pointsBinGoal
-	lastY := zeroY
-	for i := 0; i < valueCount; i++ {
-		startX := float64(i) * binWidth
-		centerX := startX + binWidth/2.0
-		endX := float64(i+1) * binWidth
-		value := float64(g.graph.DataPoints[i].Points)
-		y := zeroY - math.Min(value*valueScaling, maxHeight)
-		if i == 0 {
-			cr.LineTo(startX, y)
-			cr.LineTo(centerX, y)
-		} else {
-			cr.CurveTo(startX, lastY, startX, y, centerX, y)
-		}
-		if i == valueCount-1 {
-			cr.LineTo(endX, y)
-			cr.LineTo(endX, zeroY)
-		}
-		lastY = y
-	}
-
-	valueScaling = lowZoneHeight / g.multisBinGoal
-	lastY = zeroY
-	for i := valueCount - 1; i >= 0; i-- {
-		startX := float64(i+1) * binWidth
-		centerX := startX - binWidth/2.0
-		endX := float64(i) * binWidth
-		value := float64(g.graph.DataPoints[i].Multis)
-		y := zeroY + math.Min(value*valueScaling, maxHeight)
-		if i == valueCount-1 {
-			cr.LineTo(startX, y)
-			cr.LineTo(centerX, y)
-		} else {
-			cr.CurveTo(startX, lastY, startX, y, centerX, y)
-		}
-		if i == 0 {
-			cr.LineTo(endX, y)
-			cr.LineTo(endX, zeroY)
-		}
-		lastY = y
-	}
-	cr.ClosePath()
-	cr.Fill()
+	// TODO: draw the stacked band graph
+	g.drawDataPoints(cr, layout, g.graph.DataPoints)
 
 	// the time frame
 	if g.timeFrameIndex >= 0 && valueCount > 1 {
-		startX := float64(g.timeFrameIndex) * binWidth
-		endX := float64(g.timeFrameIndex+1) * binWidth
+		startX := float64(g.timeFrameIndex) * layout.binWidth
+		endX := float64(g.timeFrameIndex+1) * layout.binWidth
 		cr.SetSourceRGBA(rateStyle.timeFrameColor.toRGBA(rateStyle.timeFrameAlpha)) // TODO calculate the achievment of the current time frame and use the corresponding color
-		cr.MoveTo(startX, zeroY-maxHeight)
-		cr.LineTo(endX, zeroY-maxHeight)
-		cr.LineTo(endX, zeroY+maxHeight)
-		cr.LineTo(startX, zeroY+maxHeight)
+		cr.MoveTo(startX, layout.zeroY-layout.maxHeight)
+		cr.LineTo(endX, layout.zeroY-layout.maxHeight)
+		cr.LineTo(endX, layout.zeroY+layout.maxHeight)
+		cr.LineTo(startX, layout.zeroY+layout.maxHeight)
 		cr.ClosePath()
 		cr.Stroke()
 	}
 
 	// the zero line
 	cr.SetSourceRGB(rateStyle.axisColor.toRGB())
-	cr.MoveTo(0, zeroY)
-	cr.LineTo(width, zeroY)
+	cr.MoveTo(0, layout.zeroY)
+	cr.LineTo(layout.width, layout.zeroY)
 	cr.Stroke()
+}
+
+func (g *scoreGraph) calculateLayout(da *gtk.DrawingArea, valueCount int) graphLayout {
+	result := graphLayout{
+		width:   float64(da.GetAllocatedWidth()),
+		height:  float64(da.GetAllocatedHeight()),
+		marginY: 5.0,
+	}
+
+	result.zeroY = result.height / 2.0
+	result.maxHeight = result.zeroY - result.marginY
+	result.lowZoneHeight = result.maxHeight / 2.0
+	result.binWidth = result.width / float64(valueCount)
+
+	return result
 }
 
 func (g *scoreGraph) fillBackground(cr *cairo.Context) {
@@ -164,4 +134,56 @@ func (g *scoreGraph) fillBackground(cr *cairo.Context) {
 
 	cr.SetSourceRGB(rateStyle.backgroundColor.toRGB())
 	cr.Paint()
+}
+
+func (g *scoreGraph) drawDataPoints(cr *cairo.Context, layout graphLayout, datapoints []core.BandScore) {
+	valueCount := len(datapoints)
+
+	cr.SetSourceRGB(rateStyle.scoreGraphColor.toRGB())
+	cr.MoveTo(0, layout.zeroY)
+
+	valueScaling := layout.lowZoneHeight / g.pointsBinGoal
+	lastY := layout.zeroY
+	for i := 0; i < valueCount; i++ {
+		startX := float64(i) * layout.binWidth
+		centerX := startX + layout.binWidth/2.0
+		endX := float64(i+1) * layout.binWidth
+		value := float64(datapoints[i].Points)
+		y := layout.zeroY - math.Min(value*valueScaling, layout.maxHeight)
+		if i == 0 {
+			cr.LineTo(startX, y)
+			cr.LineTo(centerX, y)
+		} else {
+			cr.CurveTo(startX, lastY, startX, y, centerX, y)
+		}
+		if i == valueCount-1 {
+			cr.LineTo(endX, y)
+			cr.LineTo(endX, layout.zeroY)
+		}
+
+		lastY = y
+	}
+
+	valueScaling = layout.lowZoneHeight / g.multisBinGoal
+	lastY = layout.zeroY
+	for i := valueCount - 1; i >= 0; i-- {
+		startX := float64(i+1) * layout.binWidth
+		centerX := startX - layout.binWidth/2.0
+		endX := float64(i) * layout.binWidth
+		value := float64(datapoints[i].Multis)
+		y := layout.zeroY + math.Min(value*valueScaling, layout.maxHeight)
+		if i == valueCount-1 {
+			cr.LineTo(startX, y)
+			cr.LineTo(centerX, y)
+		} else {
+			cr.CurveTo(startX, lastY, startX, y, centerX, y)
+		}
+		if i == 0 {
+			cr.LineTo(endX, y)
+			cr.LineTo(endX, layout.zeroY)
+		}
+		lastY = y
+	}
+	cr.ClosePath()
+	cr.Fill()
 }

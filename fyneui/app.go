00292fffebcb7c221c0773b88986c23999ed96f6
Florian Thienel <ftl@thecodingflow.com>
2024-11-30 15:14:38 +0100

    evolve main window

--- fyneui/app.go
+++ fyneui/app.go
@@ -1,14 +1,54 @@
 package fyneui
 
 import (
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/widget"
+	"log"
+
+	"fyne.io/fyne/v2"
+	fyneapp "fyne.io/fyne/v2/app"
+
+	"github.com/ftl/hellocontest/core/app"
+	"github.com/ftl/hellocontest/core/cfg"
+	"github.com/ftl/hellocontest/core/clock"
 )
 
-func Run(version string, sponsors string, args []string) {
-	app := app.New()
-	window := app.NewWindow("Hello Contest")
+const AppID = "ft.hellocontest"
 
-	window.SetContent(widget.NewLabel("Hello Contest"))
-	window.ShowAndRun()
+func Run(version string, sponsors string, args []string) {
+	app := &application{
+		id:       AppID,
+		version:  version,
+		sponsors: sponsors,
+	}
+
+	app.app = fyneapp.NewWithID(app.id)
+
+	app.app.Lifecycle().SetOnStarted(app.activate)
+	app.app.Run()
+}
+
+type application struct {
+	id       string
+	version  string
+	sponsors string
+
+	app        fyne.App
+	mainWindow *mainWindow
+
+	controller *app.Controller
+}
+
+func (a *application) activate() {
+	configuration, err := cfg.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+	a.controller = app.NewController(a.version, clock.New(), a.app, a.runAsync, configuration, a.sponsors)
+
+	a.mainWindow = setupMainWindow(a.app.NewWindow("Hello Contest"))
+	a.mainWindow.UseDefaultWindowGeometry() // TODO: store/restore the window geometry
+	a.mainWindow.Show()
+}
+
+func (a *application) runAsync(f func()) {
+	f() // TODO: this is probably sufficient with Fyne, Fyne is thread safe afaik
 }

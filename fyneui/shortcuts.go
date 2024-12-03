9542a156dca1106fb2469502cc65a8cd5b53250f
Florian Thienel <ftl@thecodingflow.com>
2024-12-03 18:56:59 +0100

    add shortcuts
    Signed-off-by: Florian Thienel <ftl@thecodingflow.com>

---
+++ fyneui/shortcuts.go
@@ -0,0 +1,69 @@
+package fyneui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/driver/desktop"
+)
+
+type ShortcutID string
+
+const (
+	OpenShortcut ShortcutID = "open"
+	QuitShortcut ShortcutID = "quit"
+)
+
+type ShortcutConsumer interface {
+	AddShortcut(fyne.Shortcut, func(shortcut fyne.Shortcut))
+}
+
+type Shortcut struct {
+	desktop.CustomShortcut
+	ID     ShortcutID
+	Action func()
+}
+
+func (s *Shortcut) ShortcutName() string {
+	return s.CustomShortcut.ShortcutName()
+}
+
+func (s *Shortcut) AddTo(consumer ShortcutConsumer) {
+	consumer.AddShortcut(s, func(shortcut fyne.Shortcut) {
+		s.Action()
+	})
+}
+
+type ShortcutController interface {
+	Open()
+	Quit()
+}
+
+type Shortcuts struct {
+	shortcuts map[ShortcutID]*Shortcut
+}
+
+func setupShortcuts(controller ShortcutController) *Shortcuts {
+	return &Shortcuts{
+		shortcuts: map[ShortcutID]*Shortcut{
+			OpenShortcut: &Shortcut{
+				ID:             "open",
+				Action:         controller.Open,
+				CustomShortcut: desktop.CustomShortcut{Modifier: fyne.KeyModifierControl, KeyName: fyne.KeyO},
+			},
+			QuitShortcut: &Shortcut{
+				ID:             "quit",
+				Action:         controller.Quit,
+				CustomShortcut: desktop.CustomShortcut{Modifier: fyne.KeyModifierControl, KeyName: fyne.KeyQ},
+			},
+		},
+	}
+}
+
+func (s *Shortcuts) Get(id ShortcutID) fyne.Shortcut {
+	return s.shortcuts[id]
+}
+
+func (s *Shortcuts) AddTo(consumer ShortcutConsumer) {
+	for _, shortcut := range s.shortcuts {
+		shortcut.AddTo(consumer)
+	}
+}

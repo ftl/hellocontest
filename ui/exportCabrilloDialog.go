23610ea302b911b5b4c38d28c58fe5bef991fa5a
Florian Thienel <ftl@thecodingflow.com>
2024-12-24 15:09:17 +0100

    enter the required category entries in the export dialog
    Signed-off-by: Florian Thienel <ftl@thecodingflow.com>

--- ui/exportCabrilloDialog.go
+++ ui/exportCabrilloDialog.go
@@ -11,7 +11,12 @@ type exportCabrilloDialog struct {
 	controller ExportCabrilloController
 	view       *exportCabrilloView
 
-	openAfterExport bool
+	categoryAssisted bool
+	categoryBand     string
+	categoryMode     string
+	categoryOperator string
+	categoryPower    string
+	openAfterExport  bool
 }
 
 func setupExportCabrilloDialog(parent gtk.IWidget, controller ExportCabrilloController) *exportCabrilloDialog {
@@ -31,13 +36,82 @@ func (d *exportCabrilloDialog) Show() bool {
 	d.view = &exportCabrilloView{}
 	grid, _ := gtk.GridNew()
 	grid.SetOrientation(gtk.ORIENTATION_VERTICAL)
+	grid.SetHExpand(true)
+	grid.SetVExpand(true)
+	grid.SetColumnSpacing(5)
 
-	label, _ := gtk.LabelNew("Export the log as Cabrillo format.")
+	label, _ := gtk.LabelNew("Export the log as Cabrillo file.")
+	label.SetHAlign(gtk.ALIGN_START)
+	label.SetMarginBottom(10)
 	grid.Attach(label, 0, 0, 2, 1)
+
+	label, _ = gtk.LabelNew("Category")
+	label.SetHAlign(gtk.ALIGN_END)
+	grid.Attach(label, 0, 1, 1, 1)
+	d.view.categoriesCombo, _ = gtk.ComboBoxTextNew()
+	d.view.categoriesCombo.Append("", "")
+	for _, category := range d.controller.Categories() {
+		d.view.categoriesCombo.Append(category, category)
+	}
+	d.view.categoriesCombo.SetActive(0)
+	grid.Attach(d.view.categoriesCombo, 1, 1, 1, 1)
+
+	d.view.categoryAssistedCheckButton, _ = gtk.CheckButtonNewWithLabel("Assisted")
+	d.view.categoryAssistedCheckButton.SetActive(d.categoryAssisted)
+	grid.Attach(d.view.categoryAssistedCheckButton, 1, 2, 1, 1)
+
+	label, _ = gtk.LabelNew("Band")
+	label.SetHAlign(gtk.ALIGN_END)
+	grid.Attach(label, 0, 3, 1, 1)
+	d.view.categoryBandCombo, _ = gtk.ComboBoxTextNew()
+	d.view.categoryBandCombo.Append("", "")
+	for _, band := range d.controller.CategoryBands() {
+		d.view.categoryBandCombo.Append(band, band)
+	}
+	d.view.categoryBandCombo.SetActiveID(d.categoryBand)
+	grid.Attach(d.view.categoryBandCombo, 1, 3, 1, 1)
+
+	label, _ = gtk.LabelNew("Mode")
+	label.SetHAlign(gtk.ALIGN_END)
+	grid.Attach(label, 0, 4, 1, 1)
+	d.view.categoryModeCombo, _ = gtk.ComboBoxTextNew()
+	d.view.categoryModeCombo.Append("", "")
+	for _, mode := range d.controller.CategoryModes() {
+		d.view.categoryModeCombo.Append(mode, mode)
+	}
+	d.view.categoryModeCombo.SetActiveID(d.categoryMode)
+	grid.Attach(d.view.categoryModeCombo, 1, 4, 1, 1)
+
+	label, _ = gtk.LabelNew("Operator")
+	label.SetHAlign(gtk.ALIGN_END)
+	grid.Attach(label, 0, 5, 1, 1)
+	d.view.categoryOperatorCombo, _ = gtk.ComboBoxTextNew()
+	d.view.categoryOperatorCombo.Append("", "")
+	for _, operator := range d.controller.CategoryOperators() {
+		d.view.categoryOperatorCombo.Append(operator, operator)
+	}
+	d.view.categoryOperatorCombo.SetActiveID(d.categoryOperator)
+	grid.Attach(d.view.categoryOperatorCombo, 1, 5, 1, 1)
+
+	label, _ = gtk.LabelNew("Power")
+	label.SetHAlign(gtk.ALIGN_END)
+	grid.Attach(label, 0, 6, 1, 1)
+	d.view.categoryPowerCombo, _ = gtk.ComboBoxTextNew()
+	d.view.categoryPowerCombo.Append("", "")
+	for _, power := range d.controller.CategoryPowers() {
+		d.view.categoryPowerCombo.Append(power, power)
+	}
+	d.view.categoryPowerCombo.SetActiveID(d.categoryPower)
+	grid.Attach(d.view.categoryPowerCombo, 1, 6, 1, 1)
+
+	separator, _ := gtk.SeparatorNew(gtk.ORIENTATION_HORIZONTAL)
+	separator.SetHExpand(true)
+	separator.SetVExpand(true)
+	grid.Attach(separator, 0, 7, 2, 1)
 
 	d.view.openAfterExportCheckButton, _ = gtk.CheckButtonNewWithLabel("Open the file after export")
 	d.view.openAfterExportCheckButton.SetActive(d.openAfterExport)
-	grid.Attach(d.view.openAfterExportCheckButton, 0, 1, 2, 1)
+	grid.Attach(d.view.openAfterExportCheckButton, 0, 8, 2, 1)
 
 	d.view.setup(d.controller)
 
@@ -47,7 +121,7 @@ func (d *exportCabrilloDialog) Show() bool {
 	d.dialog.SetTransientFor(nil)
 	d.dialog.SetPosition(gtk.WIN_POS_CENTER)
 	d.dialog.Connect("destroy", d.onDestroy)
-	d.dialog.SetTitle("Export Log as Cabrillo")
+	d.dialog.SetTitle("Export Cabrillo")
 	d.dialog.SetDefaultResponse(gtk.RESPONSE_OK)
 	d.dialog.SetModal(true)
 	contentArea, _ := d.dialog.GetContentArea()
@@ -63,6 +137,41 @@ func (d *exportCabrilloDialog) Show() bool {
 	d.view = nil
 
 	return result
+}
+
+func (d *exportCabrilloDialog) SetCategoryAssisted(assisted bool) {
+	d.categoryAssisted = assisted
+	if d.view != nil {
+		d.view.categoryAssistedCheckButton.SetActive(assisted)
+	}
+}
+
+func (d *exportCabrilloDialog) SetCategoryBand(band string) {
+	d.categoryBand = band
+	if d.view != nil {
+		d.view.categoryBandCombo.SetActiveID(band)
+	}
+}
+
+func (d *exportCabrilloDialog) SetCategoryMode(mode string) {
+	d.categoryMode = mode
+	if d.view != nil {
+		d.view.categoryModeCombo.SetActiveID(mode)
+	}
+}
+
+func (d *exportCabrilloDialog) SetCategoryOperator(operator string) {
+	d.categoryOperator = operator
+	if d.view != nil {
+		d.view.categoryOperatorCombo.SetActiveID(operator)
+	}
+}
+
+func (d *exportCabrilloDialog) SetCategoryPower(power string) {
+	d.categoryPower = power
+	if d.view != nil {
+		d.view.categoryPowerCombo.SetActiveID(power)
+	}
 }
 
 func (d *exportCabrilloDialog) SetOpenAfterExport(open bool) {
--- ui/exportCabrilloView.go
+++ ui/exportCabrilloView.go
@@ -3,18 +3,66 @@ package ui
 import "github.com/gotk3/gotk3/gtk"
 
 type ExportCabrilloController interface {
+	Categories() []string
+	CategoryBands() []string
+	CategoryModes() []string
+	CategoryOperators() []string
+	CategoryPowers() []string
+
+	SetCategory(string)
+	SetCategoryAssisted(bool)
+	SetCategoryBand(string)
+	SetCategoryMode(string)
+	SetCategoryOperator(string)
+	SetCategoryPower(string)
 	SetOpenAfterExport(bool)
 }
 
 type exportCabrilloView struct {
 	controller ExportCabrilloController
 
-	openAfterExportCheckButton *gtk.CheckButton
+	categoriesCombo             *gtk.ComboBoxText
+	categoryAssistedCheckButton *gtk.CheckButton
+	categoryBandCombo           *gtk.ComboBoxText
+	categoryModeCombo           *gtk.ComboBoxText
+	categoryOperatorCombo       *gtk.ComboBoxText
+	categoryPowerCombo          *gtk.ComboBoxText
+	openAfterExportCheckButton  *gtk.CheckButton
 }
 
 func (v *exportCabrilloView) setup(controller ExportCabrilloController) {
 	v.controller = controller
+	v.categoriesCombo.Connect("changed", v.onCategoryChanged)
+	v.categoryAssistedCheckButton.Connect("toggled", v.onCategoryAssistedToggled)
+	v.categoryBandCombo.Connect("changed", v.onCategoryBandChanged)
+	v.categoryModeCombo.Connect("changed", v.onCategoryModeChanged)
+	v.categoryOperatorCombo.Connect("changed", v.onCategoryOperatorChanged)
+	v.categoryPowerCombo.Connect("changed", v.onCategoryPowerChanged)
 	v.openAfterExportCheckButton.Connect("toggled", v.onOpenAfterExportToggled)
+}
+
+func (v *exportCabrilloView) onCategoryChanged() {
+	v.controller.SetCategory(v.categoriesCombo.GetActiveText())
+}
+
+func (v *exportCabrilloView) onCategoryAssistedToggled() {
+	v.controller.SetCategoryAssisted(v.categoryAssistedCheckButton.GetActive())
+}
+
+func (v *exportCabrilloView) onCategoryBandChanged() {
+	v.controller.SetCategoryBand(v.categoryBandCombo.GetActiveText())
+}
+
+func (v *exportCabrilloView) onCategoryModeChanged() {
+	v.controller.SetCategoryMode(v.categoryModeCombo.GetActiveText())
+}
+
+func (v *exportCabrilloView) onCategoryOperatorChanged() {
+	v.controller.SetCategoryOperator(v.categoryOperatorCombo.GetActiveText())
+}
+
+func (v *exportCabrilloView) onCategoryPowerChanged() {
+	v.controller.SetCategoryPower(v.categoryPowerCombo.GetActiveText())
 }
 
 func (v *exportCabrilloView) onOpenAfterExportToggled() {

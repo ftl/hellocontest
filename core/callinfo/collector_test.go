4ff21b689857b6fe88c20f08c14874ac6345ed31
Florian Thienel <ftl@thecodingflow.com>
2025-03-22 21:04:29 +0100

    test Collector.AddCallsign

--- core/callinfo/collector_test.go
+++ core/callinfo/collector_test.go
@@ -1,1 +1,44 @@
 package callinfo
+
+import (
+	"testing"
+
+	"github.com/ftl/hamradio/callsign"
+	"github.com/ftl/hellocontest/core"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCollector_addCallsign(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    core.Callinfo
+		expected core.Callinfo
+		failure  bool
+	}{
+		{
+			name:     "empty input",
+			input:    core.Callinfo{},
+			expected: core.Callinfo{},
+			failure:  true,
+		},
+		{
+			name:     "valid callsign",
+			input:    core.Callinfo{Input: "DL1ABC"},
+			expected: core.Callinfo{Input: "DL1ABC", Call: callsign.MustParse("DL1ABC"), CallValid: true},
+		},
+		{
+			name:     "incomplete callsign",
+			input:    core.Callinfo{Input: "DL"},
+			expected: core.Callinfo{Input: "DL"},
+			failure:  true,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c := &Collector{}
+			success := c.addCallsign(&test.input)
+			assert.NotEqual(t, test.failure, success)
+			assert.Equal(t, test.expected, test.input)
+		})
+	}
+}

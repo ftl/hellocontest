22ac467b4533ebbbb9d4ab489bfe6f72c7d004c8
Florian Thienel <ftl@thecodingflow.com>
2025-04-14 22:40:30 +0200

    make each screenshot optional

--- script/screenshots.go
+++ script/screenshots.go
@@ -17,34 +17,58 @@ import (
 const ScreenshotsFolder = "./docs/screenshots"
 
 var ScreenshotsScript = &Script{
-	steps: []Step{
-		ClearScreenshotsFolder(),
-		Wait(2 * time.Second),
-		DescribeScreenshot("about dialog", 1*time.Second),
-		func(_ context.Context, app *app.Controller, ui func(func())) time.Duration {
-			ui(app.About)
-			return 0
+	sections: []*Section{
+		{
+			steps: []Step{
+				Wait(2 * time.Second),
+			},
 		},
-		TriggerScreenshot("about"),
-		Wait(2 * time.Second),
-		DescribeScreenshot("file menu, highlight QUIT", 10*time.Second),
-		TriggerScreenshot("menu_file_quit"),
-		func(_ context.Context, app *app.Controller, ui func(func())) time.Duration {
-			ui(func() {
-				app.Entry.EntrySelected(core.BandmapEntry{
-					Call: callsign.MustParse("DL3NEY"),
-				})
-			})
-			return 0
+		{
+			enter: AskForScreenshot("about dialog", 1*time.Second),
+			steps: []Step{
+				func(_ context.Context, app *app.Controller, ui func(func())) time.Duration {
+					ui(app.About)
+					return 0
+				},
+				TriggerScreenshot("about"),
+				Wait(5 * time.Second),
+			},
 		},
-		DescribeScreenshot("main window with data entry", 0),
-		TriggerScreenshot("main_window_data"),
-		Describe("all screenshots taken, closing the application", 0),
-		func(_ context.Context, app *app.Controller, ui func(func())) time.Duration {
-			ui(app.Quit)
-			return 0
+		{
+			enter: AskForScreenshot("file menu, highlight QUIT", 10*time.Second),
+			steps: []Step{
+				TriggerScreenshot("menu_file_quit"),
+				Wait(5 * time.Second),
+			},
+		},
+		{
+			enter: AskForScreenshot("main window with data entry", 0),
+			steps: []Step{
+				func(_ context.Context, app *app.Controller, ui func(func())) time.Duration {
+					ui(func() {
+						app.Entry.EntrySelected(core.BandmapEntry{
+							Call: callsign.MustParse("DL3NEY"),
+						})
+					})
+					return 0
+				},
+				TriggerScreenshot("main_window_data"),
+			},
+		},
+		{
+			steps: []Step{
+				Describe("all screenshots taken, closing the application", 0),
+				func(_ context.Context, app *app.Controller, ui func(func())) time.Duration {
+					ui(app.Quit)
+					return 0
+				},
+			},
 		},
 	},
+}
+
+func AskForScreenshot(description string, delay time.Duration) Condition {
+	return Ask("[SCREENSHOT]\n\n"+description, delay)
 }
 
 func DescribeScreenshot(description string, delay time.Duration) Step {
@@ -53,6 +77,7 @@ func DescribeScreenshot(description string, delay time.Duration) Step {
 
 func ClearScreenshotsFolder() Step {
 	return func(_ context.Context, _ *app.Controller, _ func(func())) time.Duration {
+		log.Printf("[clearing screenshots folder]")
 		d, err := os.Open(ScreenshotsFolder)
 		if err != nil {
 			log.Printf("Cannot open screenshots folder: %v", err)
@@ -76,20 +101,37 @@ func ClearScreenshotsFolder() Step {
 	}
 }
 
+func DeleteScreenshot(name string) Step {
+	return func(_ context.Context, _ *app.Controller, _ func(func())) time.Duration {
+		filename := filepath.Join(ScreenshotsFolder, name)
+		err := os.RemoveAll(filename)
+		if err != nil {
+			log.Printf("Cannot delete %s: %v", filename, err)
+		}
+		return 0
+	}
+}
+
 func TriggerScreenshot(filename string) Step {
 	return TriggerScreenshotWithDelay(filename, 0)
 }
 
-func TriggerScreenshotWithDelay(filename string, delay time.Duration) Step {
+func TriggerScreenshotWithDelay(name string, delay time.Duration) Step {
 	return func(_ context.Context, _ *app.Controller, _ func(func())) time.Duration {
+		filename := filepath.Join(ScreenshotsFolder, name+".png")
+		err := os.RemoveAll(filename)
+		if err != nil {
+			log.Printf("Cannot delete %s: %v", filename, err)
+		}
+
 		// TODO: evaluate ctx.Done() and stop the flameshot process
 		cmd := exec.Command("flameshot", "gui")
-		cmd.Args = append(cmd.Args, "--path", filepath.Join(ScreenshotsFolder, filename+".png"))
+		cmd.Args = append(cmd.Args, "--path", filename)
 		if delay > 0 {
 			cmd.Args = append(cmd.Args, "--delay", fmt.Sprintf("%d", delay.Milliseconds()))
 		}
 
-		err := cmd.Run()
+		err = cmd.Run()
 		if err != nil {
 			log.Printf("Screenshot failed: %v", err)
 		} else {
--- script/script.go
+++ script/script.go
@@ -8,17 +8,64 @@ import (
 	"github.com/ftl/hellocontest/core/app"
 )
 
-type Step func(ctx context.Context, app *app.Controller, ui func(func())) time.Duration
-
 type Script struct {
-	steps []Step
+	sections []*Section
 
-	waitUntil time.Time
-	nextStep  int
+	currentSection int
 }
 
+type Section struct {
+	enter Condition
+	steps []Step
+
+	waitUntil   time.Time
+	currentStep int
+}
+
+type Step func(ctx context.Context, app *app.Controller, ui func(func())) time.Duration
+
+type Condition func(ctx context.Context, app *app.Controller, ui func(func())) (bool, time.Duration)
+
 func (s *Script) Step(ctx context.Context, app *app.Controller, ui func(func())) bool {
-	if s.nextStep >= len(s.steps) {
+	if s.currentSection >= len(s.sections) {
+		return false
+	}
+
+	section := s.sections[s.currentSection]
+
+	cont := section.Step(ctx, app, ui)
+	if !cont {
+		s.currentSection += 1
+	}
+
+	return true
+}
+
+func (s *Section) Step(ctx context.Context, app *app.Controller, ui func(func())) bool {
+	if s.currentStep == 0 {
+		return s.checkEntryCondition(ctx, app, ui)
+	}
+
+	return s.executeStep(ctx, app, ui)
+}
+
+func (s *Section) checkEntryCondition(ctx context.Context, app *app.Controller, ui func(func())) bool {
+	s.currentStep += 1
+	if s.enter == nil {
+		return true
+	}
+
+	now := time.Now()
+	enter, delay := s.enter(ctx, app, ui)
+	if delay != 0 {
+		s.waitUntil = now.Add(delay)
+	}
+
+	return enter
+}
+
+func (s *Section) executeStep(ctx context.Context, app *app.Controller, ui func(func())) bool {
+	if s.currentStep > len(s.steps) {
 		return false
 	}
 
@@ -27,11 +74,25 @@ func (s *Script) Step(ctx context.Context, app *app.Controller, ui func(func()))
 		return true
 	}
 
-	nextDuration := s.steps[s.nextStep](ctx, app, ui)
+	step := s.steps[s.currentStep-1]
+
+	nextDuration := step(ctx, app, ui)
 	s.waitUntil = now.Add(nextDuration)
-	s.nextStep += 1
+	s.currentStep += 1
 
 	return true
+}
+
+func Ask(description string, delay time.Duration) Condition {
+	return func(_ context.Context, app *app.Controller, ui func(func())) (bool, time.Duration) {
+		enter := make(chan bool, 1)
+		defer close(enter)
+		ui(func() {
+			enter <- app.ShowQuestion("%s\n\nin %v", description, delay)
+		})
+		doEnter := <-enter
+		return doEnter, delay
+	}
 }
 
 func Describe(description string, delay time.Duration) Step {

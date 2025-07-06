a4821e2ee07ca327fc6d0a8d339cc3a4f6e48adf
Florian Thienel <ftl@thecodingflow.com>
2025-07-06 12:58:30 +0200

    reimplement the scoreWindow widgets without using the Glade file

--- ui/scoreView.go
+++ ui/scoreView.go
@@ -10,22 +10,45 @@ import (
 )
 
 type scoreView struct {
+	rootGrid   *gtk.Grid
 	tableLabel *gtk.Label
 	graphArea  *gtk.DrawingArea
 
 	graph *scoreGraph
 }
 
-func setupNewScoreView(builder *gtk.Builder, colors colorProvider, clock core.Clock) *scoreView {
+func setupNewScoreView(parent gtk.IWidget, colors colorProvider, clock core.Clock) *scoreView {
 	result := &scoreView{}
 
-	result.tableLabel = getUI(builder, "tableLabel").(*gtk.Label)
-	style.AddClass(result.tableLabel.ToWidget(), "score_table")
+	result.rootGrid, _ = gtk.GridNew()
+	result.rootGrid.InsertColumn(0)
+	result.rootGrid.InsertRow(0)
+	result.rootGrid.InsertRow(0)
+	result.rootGrid.SetColumnSpacing(5)
+	result.rootGrid.SetRowSpacing(5)
+	result.rootGrid.SetCanFocus(false)
 
 	result.graph = newScoreGraph(colors, clock)
-	result.graphArea = getUI(builder, "scoreGraphArea").(*gtk.DrawingArea)
+	result.graphArea, _ = gtk.DrawingAreaNew()
+	result.graphArea.SetHExpand(true)
+	result.graphArea.SetVExpand(true)
+	result.graphArea.SetHAlign(gtk.ALIGN_FILL)
+	result.graphArea.SetVAlign(gtk.ALIGN_FILL)
+	result.graphArea.SetCanFocus(false)
 	result.graphArea.Connect("draw", result.graph.Draw)
 	result.graphArea.Connect("style-updated", result.graph.RefreshStyle)
+
+	result.tableLabel, _ = gtk.LabelNew("")
+	result.tableLabel.SetHExpand(true)
+	result.tableLabel.SetVExpand(false)
+	result.tableLabel.SetHAlign(gtk.ALIGN_FILL)
+	result.tableLabel.SetVAlign(gtk.ALIGN_FILL)
+	result.tableLabel.SetTrackVisitedLinks(false)
+	result.tableLabel.SetCanFocus(false)
+	style.AddClass(result.tableLabel.ToWidget(), "score_table")
+
+	result.rootGrid.Attach(result.graphArea, 0, 0, 1, 1)
+	result.rootGrid.Attach(result.tableLabel, 0, 1, 1, 1)
 
 	return result
 }

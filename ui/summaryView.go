020fefd50204d07ec15431871edb01d24ce5f8da
Florian Thienel <ftl@thecodingflow.com>
2025-07-27 19:10:02 +0200

    use a two column layout for the summary dialog

--- ui/summaryView.go
+++ ui/summaryView.go
@@ -56,35 +56,58 @@ func newSummaryView(controller SummaryController) *summaryView {
 	result.root.SetMarginStart(5)
 	result.root.SetMarginEnd(5)
 
-	result.contestNameEntry = buildLabeledEntry(result.root, 0, "Contest Name", nil)
-	result.cabrilloNameEntry = buildLabeledEntry(result.root, 1, "Cabrillo Name", nil)
-	result.startTimeEntry = buildLabeledEntry(result.root, 2, "Start Time", nil)
-	result.callsignEntry = buildLabeledEntry(result.root, 3, "Callsign", nil)
-	result.myExchangesEntry = buildLabeledEntry(result.root, 4, "My Exchanges", nil)
+	columns, _ := gtk.GridNew()
+	columns.SetOrientation(gtk.ORIENTATION_HORIZONTAL)
+	columns.SetHExpand(true)
+	columns.SetVExpand(false)
+	columns.SetColumnSpacing(20)
+	result.root.Attach(columns, 0, 1, 1, 1)
 
-	buildSeparator(result.root, 5, 2)
+	leftColumn, _ := gtk.GridNew()
+	leftColumn.SetOrientation(gtk.ORIENTATION_VERTICAL)
+	leftColumn.SetHExpand(true)
+	leftColumn.SetVExpand(false)
+	leftColumn.SetColumnSpacing(5)
+	leftColumn.SetRowSpacing(5)
+	columns.Attach(leftColumn, 0, 0, 1, 1)
 
-	result.operatorModeCombo = buildLabeledCombo(result.root, 6, "Operator Mode", false, result.controller.OperatorModes(), result.onOperatorModeChanged)
-	result.overlayCombo = buildLabeledCombo(result.root, 7, "Overlay", false, result.controller.Overlays(), result.onOverlayChanged)
-	result.powerModeCombo = buildLabeledCombo(result.root, 8, "Power", false, result.controller.PowerModes(), result.onPowerModeChanged)
-	result.assistedCheckButton = buildCheckButtonInColumn(result.root, 9, 1, 1, "Assisted", result.onAssistedToggled)
+	rightColumn, _ := gtk.GridNew()
+	rightColumn.SetOrientation(gtk.ORIENTATION_VERTICAL)
+	rightColumn.SetHExpand(true)
+	rightColumn.SetVExpand(false)
+	rightColumn.SetColumnSpacing(5)
+	rightColumn.SetRowSpacing(5)
+	columns.Attach(rightColumn, 1, 0, 1, 1)
 
-	buildSeparator(result.root, 10, 2)
+	// left
 
-	result.workedModesEntry = buildLabeledEntry(result.root, 11, "Worked Modes", nil)
-	result.workedBandsEntry = buildLabeledEntry(result.root, 12, "Worked Bands", nil)
-	result.operatingTimeEntry = buildLabeledEntry(result.root, 13, "Operating Time", nil)
-	result.breakTimeEntry = buildLabeledEntry(result.root, 14, "Break Time", nil)
-	result.breaksEntry = buildLabeledEntry(result.root, 15, "Breaks", nil)
+	result.contestNameEntry = buildLabeledEntry(leftColumn, 0, "Contest Name", nil)
+	result.cabrilloNameEntry = buildLabeledEntry(leftColumn, 1, "Cabrillo Name", nil)
+	result.startTimeEntry = buildLabeledEntry(leftColumn, 2, "Start Time", nil)
+	result.callsignEntry = buildLabeledEntry(leftColumn, 3, "Callsign", nil)
+	result.myExchangesEntry = buildLabeledEntry(leftColumn, 4, "My Exchanges", nil)
 
-	buildSeparator(result.root, 16, 2)
+	buildHeaderLabel(leftColumn, 5, "Working Condition")
+	result.operatorModeCombo = buildLabeledCombo(leftColumn, 6, "Operator Mode", false, result.controller.OperatorModes(), result.onOperatorModeChanged)
+	result.overlayCombo = buildLabeledCombo(leftColumn, 7, "Overlay", false, result.controller.Overlays(), result.onOverlayChanged)
+	result.powerModeCombo = buildLabeledCombo(leftColumn, 8, "Power", false, result.controller.PowerModes(), result.onPowerModeChanged)
+	result.assistedCheckButton = buildCheckButtonInColumn(leftColumn, 9, 1, 1, "Assisted", result.onAssistedToggled)
 
+	//right
+
+	result.workedModesEntry = buildLabeledEntry(rightColumn, 0, "Worked Modes", nil)
+	result.workedBandsEntry = buildLabeledEntry(rightColumn, 1, "Worked Bands", nil)
+	result.operatingTimeEntry = buildLabeledEntry(rightColumn, 2, "Operating Time", nil)
+	result.breakTimeEntry = buildLabeledEntry(rightColumn, 3, "Break Time", nil)
+	result.breaksEntry = buildLabeledEntry(rightColumn, 4, "Breaks", nil)
+
+	buildHeaderLabel(rightColumn, 5, "Claimed Score")
 	result.scoreTable = newScoreTable(nil)
-	result.root.Attach(result.scoreTable.Table(), 0, 17, 2, 1)
+	rightColumn.Attach(result.scoreTable.Table(), 0, 6, 2, 1)
 
-	buildSeparator(result.root, 18, 2)
+	buildSeparator(result.root, 2, 1)
 
-	result.openAfterExportCheckButton = buildCheckButtonInColumn(result.root, 19, 0, 2, "Open the file after export", result.onOpenAfterExportToggled)
+	result.openAfterExportCheckButton = buildCheckButton(result.root, 3, "Open the file after export", result.onOpenAfterExportToggled)
 
 	return result
 }

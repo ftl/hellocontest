685307ec55db76e11e074461033cd58d478105a4
Florian Thienel <ftl@thecodingflow.com>
2025-07-27 18:54:40 +0200

    export the summary to a text file

--- core/export/summary/export.go
+++ core/export/summary/export.go
@@ -1,12 +1,40 @@
 package summary
 
 import (
-	"fmt"
+	"html/template"
 	"io"
+	"strings"
 
 	"github.com/ftl/hellocontest/core"
 )
 
 func Export(w io.Writer, summary core.Summary) error {
-	return fmt.Errorf("summary.Export is not yet implemented")
+	return summaryTemplate.Execute(w, summary)
 }
+
+var summaryTemplate = template.Must(template.New("summary").
+	Funcs(template.FuncMap{
+		"join":     strings.Join,
+		"datetime": core.FormatTimestamp,
+		"duration": core.FormatDuration,
+	}).
+	Parse(`Contest:    {{.ContestName}}
+Cabrillo:   {{.CabrilloName}}
+Start Time: {{datetime .StartTime}}
+Callsign:   {{.Callsign}}
+{{- if .MyExchanges}}
+Exchange:   {{.MyExchanges}}
+{{- end}}
+
+{{if .WorkingConditions -}}
+Working Conditions: {{.WorkingConditions}}
+{{end -}}
+Worked Modes:       {{join .WorkedModes ", "}}
+Worked Bands:       {{join .WorkedBands ", "}}
+Operating Time:     {{duration .TimeReport.OperationTime}}
+
+Claimed Score:
+{{.Score}}
+
+created with Hello Contest - https://github.com/ftl/hellocontest
+`))

666c0b1a91915cc929eddf1db50d38bd8c50fca9
Florian Thienel <ftl@thecodingflow.com>
2025-12-30 13:15:57 +0100

    add a Zero clock

--- core/clock/clock.go
+++ core/clock/clock.go
@@ -9,17 +9,17 @@ func New() *DefaultClock {
 	return new(DefaultClock)
 }
 
+type DefaultClock struct{}
+
+func (*DefaultClock) Now() time.Time {
+	return time.Now()
+}
+
 // Static returns a new clock that always returns the given static value.
 func Static(time time.Time) *StaticClock {
 	return &StaticClock{
 		time: time,
 	}
-}
-
-type DefaultClock struct{}
-
-func (*DefaultClock) Now() time.Time {
-	return time.Now()
 }
 
 type StaticClock struct {
@@ -28,4 +28,14 @@ type StaticClock struct {
 
 func (c *StaticClock) Now() time.Time {
 	return c.time
+}
+
+func Zero() *ZeroClock {
+	return &ZeroClock{}
+}
+
+type ZeroClock struct{}
+
+func (c *ZeroClock) Now() time.Time {
+	return time.Time{}
 }

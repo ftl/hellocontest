7ec7ceb99c60d3639bd3e6d4568e151afaa46a42
Florian Thienel <ftl@thecodingflow.com>
2026-06-07 12:49:08 +0200

    remove unused whitespace

--- ui/centralArea.go
+++ ui/centralArea.go
@@ -80,47 +80,43 @@ func (a *centralArea) SetExchangeFields(myExchangeFields, theirExchangeFields []
 }
 
 func (a *centralArea) addWidgetsToLayout() {
-	lastColumn := 4 + len(a.entry.theirExchangeFields)
+	lastColumn := 3 + len(a.entry.theirExchangeFields)
 
 	// row 0: my data: call, exchange
-	a.entryLayout.AddWidget2(a.entry.utcLabel.QWidget, 0, 0)
-	a.entryLayout.AddWidget2(a.entry.myCallLabel.QWidget, 0, 1)
+	a.entryLayout.AddWidget2(a.entry.myCallLabel.QWidget, 0, 0)
 	for i := range a.entry.myExchangeFields {
-		a.entryLayout.AddWidget2(a.entry.myExchangeFields[i].QWidget, 0, i+2)
+		a.entryLayout.AddWidget2(a.entry.myExchangeFields[i].QWidget, 0, i+1)
 	}
 
 	// row 1: horizontal separator
 	a.entryLayout.AddWidget3(a.entry.topSeparator.QWidget, 1, 0, 1, -1)
 
 	// row 2: Frequency, Band, Mode, [Serial Claim], XIT, TRX
-	a.entryLayout.AddWidget2(a.entry.vfoLabel.QWidget, 2, 0)
-	a.entryLayout.AddWidget2(a.entry.frequencyLabel.QWidget, 2, 1)
-	a.entryLayout.AddWidget2(a.entry.bandModeContainer, 2, 2)
+	a.entryLayout.AddWidget3(a.entry.vfo1Container, 2, 0, 1, 2)
 	if a.entry.serialClaimLabel != nil {
-		a.entryLayout.AddWidget2(a.entry.serialClaimLabel.QWidget, 2, 3)
+		a.entryLayout.AddWidget2(a.entry.serialClaimLabel.QWidget, 2, 2)
 	}
 	a.entryLayout.AddWidget2(a.entry.xit.QWidget, 2, lastColumn-1)
 	a.entryLayout.AddWidget2(a.entry.txIndicator.QWidget, 2, lastColumn)
 
 	// row 3: callinfo: best match, exchange, value, qtcs
-	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO1].callsignLabel.QWidget, 3, 1)
+	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO1].callsignLabel.QWidget, 3, 0)
 	for i := range a.callinfo.vfo[core.VFO1].predictedExchangeLabels {
-		a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO1].predictedExchangeLabels[i].QWidget, 3, i+2)
+		a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO1].predictedExchangeLabels[i].QWidget, 3, i+1)
 	}
 	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO1].valueLabel.QWidget, 3, lastColumn-1)
 	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO1].qtcStatusLabel.QWidget, 3, lastColumn)
 
 	// row 4: their data: call, exchange, log-button, clear-button
-	a.entryLayout.AddWidget2(a.entry.theirLabel.QWidget, 4, 0)
-	a.entryLayout.AddWidget2(a.entry.callsign.QWidget, 4, 1)
+	a.entryLayout.AddWidget2(a.entry.callsign.QWidget, 4, 0)
 	for i := range a.entry.theirExchangeFields {
-		a.entryLayout.AddWidget2(a.entry.theirExchangeFields[i].QWidget, 4, i+2)
+		a.entryLayout.AddWidget2(a.entry.theirExchangeFields[i].QWidget, 4, i+1)
 	}
 	a.entryLayout.AddWidget2(a.entry.logButton.QWidget, 4, lastColumn-1)
 	a.entryLayout.AddWidget2(a.entry.clearButton.QWidget, 4, lastColumn)
 
 	// row 5: supercheck
-	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO1].supercheckLabel.QWidget, 5, 1, 1, -1)
+	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO1].supercheckLabel.QWidget, 5, 0, 1, -1)
 
 	// row 6: dxcc, personal info
 	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO1].infoContainer, 6, 0, 1, -1)
@@ -132,34 +128,31 @@ func (a *centralArea) addWidgetsToLayout() {
 	a.entryLayout.AddWidget3(a.entry.vfoSeparator.QWidget, 8, 0, 1, -1)
 
 	// row 9: VFO2 frequency / band / mode / [serial claim] / XIT / TX
-	a.entryLayout.AddWidget2(a.entry.vfo2Label.QWidget, 9, 0)
-	a.entryLayout.AddWidget2(a.entry.vfo2FrequencyLabel.QWidget, 9, 1)
-	a.entryLayout.AddWidget2(a.entry.vfo2BandModeContainer, 9, 2)
+	a.entryLayout.AddWidget3(a.entry.vfo2Container, 9, 0, 1, 2)
 	if a.entry.vfo2SerialClaimLabel != nil {
-		a.entryLayout.AddWidget2(a.entry.vfo2SerialClaimLabel.QWidget, 9, 3)
+		a.entryLayout.AddWidget2(a.entry.vfo2SerialClaimLabel.QWidget, 9, 2)
 	}
 	a.entryLayout.AddWidget2(a.entry.vfo2XITIndicator.QWidget, 9, lastColumn-1)
 	a.entryLayout.AddWidget2(a.entry.vfo2TXIndicator.QWidget, 9, lastColumn)
 
 	// row 10: VFO2 callinfo: best match, predicted exchange, value, qtcs
-	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].callsignLabel.QWidget, 10, 1)
+	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].callsignLabel.QWidget, 10, 0)
 	for i := range a.callinfo.vfo[core.VFO2].predictedExchangeLabels {
-		a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].predictedExchangeLabels[i].QWidget, 10, i+2)
+		a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].predictedExchangeLabels[i].QWidget, 10, i+1)
 	}
 	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].valueLabel.QWidget, 10, lastColumn-1)
 	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].qtcStatusLabel.QWidget, 10, lastColumn)
 
 	// row 11: VFO2 their data: call, exchange, log, clear
-	a.entryLayout.AddWidget2(a.entry.vfo2TheirLabel.QWidget, 11, 0)
-	a.entryLayout.AddWidget2(a.entry.vfo2Callsign.QWidget, 11, 1)
+	a.entryLayout.AddWidget2(a.entry.vfo2Callsign.QWidget, 11, 0)
 	for i := range a.entry.vfo2TheirExchangeFields {
-		a.entryLayout.AddWidget2(a.entry.vfo2TheirExchangeFields[i].QWidget, 11, i+2)
+		a.entryLayout.AddWidget2(a.entry.vfo2TheirExchangeFields[i].QWidget, 11, i+1)
 	}
 	a.entryLayout.AddWidget2(a.entry.vfo2LogButton.QWidget, 11, lastColumn-1)
 	a.entryLayout.AddWidget2(a.entry.vfo2ClearButton.QWidget, 11, lastColumn)
 
 	// row 12: VFO2 supercheck
-	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO2].supercheckLabel.QWidget, 12, 1, 1, -1)
+	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO2].supercheckLabel.QWidget, 12, 0, 1, -1)
 
 	// row 13: VFO2 dxcc, personal info
 	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO2].infoContainer, 13, 0, 1, -1)
@@ -182,7 +175,7 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	// row 2: Frequency, Band, Mode, [Serial Claim], XIT, TRX
 	a.entryLayout.RemoveWidget(a.entry.vfoLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.frequencyLabel.QWidget)
-	a.entryLayout.RemoveWidget(a.entry.bandModeContainer)
+	a.entryLayout.RemoveWidget(a.entry.vfo1Container)
 	if a.entry.serialClaimLabel != nil {
 		a.entryLayout.RemoveWidget(a.entry.serialClaimLabel.QWidget)
 	}
@@ -198,7 +191,6 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO1].qtcStatusLabel.QWidget)
 
 	// row 4: their data: call, exchange, log-button, clear-button
-	a.entryLayout.RemoveWidget(a.entry.theirLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.callsign.QWidget)
 	for i := range a.entry.theirExchangeFields {
 		a.entryLayout.RemoveWidget(a.entry.theirExchangeFields[i].QWidget)
@@ -221,7 +213,7 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	// row 9: VFO2
 	a.entryLayout.RemoveWidget(a.entry.vfo2Label.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.vfo2FrequencyLabel.QWidget)
-	a.entryLayout.RemoveWidget(a.entry.vfo2BandModeContainer)
+	a.entryLayout.RemoveWidget(a.entry.vfo2Container)
 	if a.entry.vfo2SerialClaimLabel != nil {
 		a.entryLayout.RemoveWidget(a.entry.vfo2SerialClaimLabel.QWidget)
 	}
@@ -237,7 +229,6 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO2].qtcStatusLabel.QWidget)
 
 	// row 11: VFO2 their data
-	a.entryLayout.RemoveWidget(a.entry.vfo2TheirLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.vfo2Callsign.QWidget)
 	for i := range a.entry.vfo2TheirExchangeFields {
 		a.entryLayout.RemoveWidget(a.entry.vfo2TheirExchangeFields[i].QWidget)

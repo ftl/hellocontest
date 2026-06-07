ffc2398f23de51335ccf083307bb6ebac8986b40
Florian Thienel <ftl@thecodingflow.com>
2026-06-07 10:55:20 +0200

    add a horizontal separator between the VFOs

--- ui/centralArea.go
+++ ui/centralArea.go
@@ -89,7 +89,7 @@ func (a *centralArea) addWidgetsToLayout() {
 		a.entryLayout.AddWidget2(a.entry.myExchangeFields[i].QWidget, 0, i+2)
 	}
 
-	// row 1: VFO label + horizontal separator
+	// row 1: horizontal separator
 	a.entryLayout.AddWidget3(a.entry.topSeparator.QWidget, 1, 0, 1, -1)
 
 	// row 2: Frequency, Band, Mode, [Serial Claim], XIT, TRX
@@ -128,41 +128,44 @@ func (a *centralArea) addWidgetsToLayout() {
 	// row 7: message
 	a.entryLayout.AddWidget3(a.entry.messageLabel.QWidget, 7, 0, 1, -1)
 
-	// row 8: VFO2 frequency / band / mode / [serial claim] / XIT / TX
-	a.entryLayout.AddWidget2(a.entry.vfo2Label.QWidget, 8, 0)
-	a.entryLayout.AddWidget2(a.entry.vfo2FrequencyLabel.QWidget, 8, 1)
-	a.entryLayout.AddWidget2(a.entry.vfo2BandModeContainer, 8, 2)
+	// row 8: horizontal separator
+	a.entryLayout.AddWidget3(a.entry.vfoSeparator.QWidget, 8, 0, 1, -1)
+
+	// row 9: VFO2 frequency / band / mode / [serial claim] / XIT / TX
+	a.entryLayout.AddWidget2(a.entry.vfo2Label.QWidget, 9, 0)
+	a.entryLayout.AddWidget2(a.entry.vfo2FrequencyLabel.QWidget, 9, 1)
+	a.entryLayout.AddWidget2(a.entry.vfo2BandModeContainer, 9, 2)
 	if a.entry.vfo2SerialClaimLabel != nil {
-		a.entryLayout.AddWidget2(a.entry.vfo2SerialClaimLabel.QWidget, 8, 3)
+		a.entryLayout.AddWidget2(a.entry.vfo2SerialClaimLabel.QWidget, 9, 3)
 	}
-	a.entryLayout.AddWidget2(a.entry.vfo2XITIndicator.QWidget, 8, lastColumn-1)
-	a.entryLayout.AddWidget2(a.entry.vfo2TXIndicator.QWidget, 8, lastColumn)
+	a.entryLayout.AddWidget2(a.entry.vfo2XITIndicator.QWidget, 9, lastColumn-1)
+	a.entryLayout.AddWidget2(a.entry.vfo2TXIndicator.QWidget, 9, lastColumn)
 
-	// row 9: VFO2 callinfo: best match, predicted exchange, value, qtcs
-	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].callsignLabel.QWidget, 9, 1)
+	// row 10: VFO2 callinfo: best match, predicted exchange, value, qtcs
+	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].callsignLabel.QWidget, 10, 1)
 	for i := range a.callinfo.vfo[core.VFO2].predictedExchangeLabels {
-		a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].predictedExchangeLabels[i].QWidget, 9, i+2)
+		a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].predictedExchangeLabels[i].QWidget, 10, i+2)
 	}
-	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].valueLabel.QWidget, 9, lastColumn-1)
-	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].qtcStatusLabel.QWidget, 9, lastColumn)
+	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].valueLabel.QWidget, 10, lastColumn-1)
+	a.entryLayout.AddWidget2(a.callinfo.vfo[core.VFO2].qtcStatusLabel.QWidget, 10, lastColumn)
 
-	// row 10: VFO2 their data: call, exchange, log, clear
-	a.entryLayout.AddWidget2(a.entry.vfo2TheirLabel.QWidget, 10, 0)
-	a.entryLayout.AddWidget2(a.entry.vfo2Callsign.QWidget, 10, 1)
+	// row 11: VFO2 their data: call, exchange, log, clear
+	a.entryLayout.AddWidget2(a.entry.vfo2TheirLabel.QWidget, 11, 0)
+	a.entryLayout.AddWidget2(a.entry.vfo2Callsign.QWidget, 11, 1)
 	for i := range a.entry.vfo2TheirExchangeFields {
-		a.entryLayout.AddWidget2(a.entry.vfo2TheirExchangeFields[i].QWidget, 10, i+2)
+		a.entryLayout.AddWidget2(a.entry.vfo2TheirExchangeFields[i].QWidget, 11, i+2)
 	}
-	a.entryLayout.AddWidget2(a.entry.vfo2LogButton.QWidget, 10, lastColumn-1)
-	a.entryLayout.AddWidget2(a.entry.vfo2ClearButton.QWidget, 10, lastColumn)
+	a.entryLayout.AddWidget2(a.entry.vfo2LogButton.QWidget, 11, lastColumn-1)
+	a.entryLayout.AddWidget2(a.entry.vfo2ClearButton.QWidget, 11, lastColumn)
 
-	// row 11: VFO2 supercheck
-	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO2].supercheckLabel.QWidget, 11, 1, 1, -1)
+	// row 12: VFO2 supercheck
+	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO2].supercheckLabel.QWidget, 12, 1, 1, -1)
 
-	// row 12: VFO2 dxcc, personal info
-	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO2].infoContainer, 12, 0, 1, -1)
+	// row 13: VFO2 dxcc, personal info
+	a.entryLayout.AddWidget3(a.callinfo.vfo[core.VFO2].infoContainer, 13, 0, 1, -1)
 
-	// row 13: VFO2 message
-	a.entryLayout.AddWidget3(a.entry.vfo2MessageLabel.QWidget, 13, 0, 1, -1)
+	// row 14: VFO2 message
+	a.entryLayout.AddWidget3(a.entry.vfo2MessageLabel.QWidget, 14, 0, 1, -1)
 }
 
 func (a *centralArea) removeWidgetsFromLayout() {
@@ -173,11 +176,11 @@ func (a *centralArea) removeWidgetsFromLayout() {
 		a.entryLayout.RemoveWidget(a.entry.myExchangeFields[i].QWidget)
 	}
 
-	// row 1: VFO label + horizontal separator
-	a.entryLayout.RemoveWidget(a.entry.vfoLabel.QWidget)
+	// row 1: horizontal separator
 	a.entryLayout.RemoveWidget(a.entry.topSeparator.QWidget)
 
 	// row 2: Frequency, Band, Mode, [Serial Claim], XIT, TRX
+	a.entryLayout.RemoveWidget(a.entry.vfoLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.frequencyLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.bandModeContainer)
 	if a.entry.serialClaimLabel != nil {
@@ -212,7 +215,10 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	// row 7: message
 	a.entryLayout.RemoveWidget(a.entry.messageLabel.QWidget)
 
-	// row 8: VFO2
+	// row 8: horizontal separator
+	a.entryLayout.RemoveWidget(a.entry.vfoSeparator.QWidget)
+
+	// row 9: VFO2
 	a.entryLayout.RemoveWidget(a.entry.vfo2Label.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.vfo2FrequencyLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.vfo2BandModeContainer)
@@ -222,7 +228,7 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	a.entryLayout.RemoveWidget(a.entry.vfo2XITIndicator.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.vfo2TXIndicator.QWidget)
 
-	// row 9: VFO2 callinfo
+	// row 10: VFO2 callinfo
 	a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO2].callsignLabel.QWidget)
 	for i := range a.callinfo.vfo[core.VFO2].predictedExchangeLabels {
 		a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO2].predictedExchangeLabels[i].QWidget)
@@ -230,7 +236,7 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO2].valueLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO2].qtcStatusLabel.QWidget)
 
-	// row 10: VFO2 their data
+	// row 11: VFO2 their data
 	a.entryLayout.RemoveWidget(a.entry.vfo2TheirLabel.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.vfo2Callsign.QWidget)
 	for i := range a.entry.vfo2TheirExchangeFields {
@@ -239,12 +245,12 @@ func (a *centralArea) removeWidgetsFromLayout() {
 	a.entryLayout.RemoveWidget(a.entry.vfo2LogButton.QWidget)
 	a.entryLayout.RemoveWidget(a.entry.vfo2ClearButton.QWidget)
 
-	// row 11: VFO2 supercheck
+	// row 12: VFO2 supercheck
 	a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO2].supercheckLabel.QWidget)
 
-	// row 12: VFO2 dxcc, personal info
+	// row 13: VFO2 dxcc, personal info
 	a.entryLayout.RemoveWidget(a.callinfo.vfo[core.VFO2].infoContainer)
 
-	// row 13: VFO2 message
+	// row 14: VFO2 message
 	a.entryLayout.RemoveWidget(a.entry.vfo2MessageLabel.QWidget)
 }
